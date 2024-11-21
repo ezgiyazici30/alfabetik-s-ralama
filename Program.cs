0b82696b123724877f39f27a63c56b0b2c52f1ae
ezgiyazici30 <ezgiyazici3011@gmail.com>
2024-11-21 14:42:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList isimler = new ArrayList();
+        isimler.Add("Zeynep");
+        isimler.Add("Ahmet");
+        isimler.Add("Mehmet");
+        isimler.Add("Ayşe");
+        isimler.Add("Elif");
+
+       
+        isimler.Sort();
+
+        
+        Console.WriteLine("Alfabetik sıralama:");
+        foreach (string isim in isimler)
+        {
+            Console.WriteLine(isim);
+        }
+    }
+}
+
